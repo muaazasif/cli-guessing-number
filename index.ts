32023f50f475791c9e70e6d027a6871cd5a6825b
muaazasif <muhammed.muaaz@gmail.com>
2024-03-24 14:54:24 +0500

    push my cli guessing number game to github

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const random = Math.floor(Math.random()*10+1);
+// console.log(random);
+
+const answer = await inquirer.prompt([
+  {
+    message: "Please Guess a Number between 1 to 10: ",
+    type: "number",
+    name: "userGuessNumber",
+  },
+]);
+if (answer.userGuessNumber === random) {
+  console.log("Congratulation You Guess Right Number!!!");
+}
+else{
+    console.log("You Guess Wrong Number!!!");
+    
+}
+console.log(answer);
