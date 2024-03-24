75ab5c6d5fb13a74cbd15150fede520e532d1330
muaazasif <muhammed.muaaz@gmail.com>
2024-03-24 15:20:27 +0500

    adding colorful and playful elements

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-const random = Math.floor(Math.random() * 10 + 1);
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
 // console.log(random);
+console.log(chalk.bold.rgb(255, 153, 0)("\n🎲 Welcome to the Guessing Game! 🎉\n"));
+console.log(chalk.yellowBright("🌟 Let's see if you can guess the magic number! 🌈\n"));
 const answer = await inquirer.prompt([
     {
-        message: "Please Guess a Number between 1 to 10: ",
+        message: chalk.magentaBright("Please Guess a Number between 1 to 10: "),
         type: "number",
         name: "userGuessNumber",
     },
 ]);
-if (answer.userGuessNumber === random) {
-    console.log("Congratulation You Guess Right Number!!!");
+if (answer.userGuessNumber === randomNumber) {
+    console.log(chalk.greenBright("\n🎉 Congratulations! You guessed the right number! 🌟\n"));
 }
 else {
-    console.log("You Guess Wrong Number!!!");
+    console.log(chalk.redBright("\n😢 Oops! Better luck next time!\n"));
+    console.log(chalk.blueBright(`💡 Hint: The correct number was ${randomNumber}.`));
 }
-console.log(answer);
+// console.log(answer);
--- index.ts
+++ index.ts
@@ -1,21 +1,33 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 
-const random = Math.floor(Math.random()*10+1);
+const randomNumber = Math.floor(Math.random() * 10 + 1);
 // console.log(random);
+console.log(
+  chalk.bold.rgb(255, 153, 0)("\n🎲 Welcome to the Guessing Game! 🎉\n")
+);
+console.log(
+  chalk.yellowBright("🌟 Let's see if you can guess the magic number! 🌈\n")
+);
 
 const answer = await inquirer.prompt([
   {
-    message: "Please Guess a Number between 1 to 10: ",
+    message: chalk.magentaBright("Please Guess a Number between 1 to 10: "),
     type: "number",
     name: "userGuessNumber",
   },
 ]);
-if (answer.userGuessNumber === random) {
-  console.log("Congratulation You Guess Right Number!!!");
+if (answer.userGuessNumber === randomNumber) {
+  console.log(
+    chalk.greenBright(
+      "\n🎉 Congratulations! You guessed the right number! 🌟\n"
+    )
+  );
+} else {
+  console.log(chalk.redBright("\n😢 Oops! Better luck next time!\n"));
+  console.log(
+    chalk.blueBright(`💡 Hint: The correct number was ${randomNumber}.`)
+  );
 }
-else{
-    console.log("You Guess Wrong Number!!!");
-    
-}
-console.log(answer);
+// console.log(answer);
